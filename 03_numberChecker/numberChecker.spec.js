a879c25f7f79fc19eb04178f97aca49742a8cd25
Fractal <150508387+harrythar88@users.noreply.github.com>
2025-11-23 16:15:11 +0900

    Update numberChecker.spec.js

--- 03_numberChecker/numberChecker.spec.js
+++ 03_numberChecker/numberChecker.spec.js
@@ -2,15 +2,15 @@ const numberChecker = require('./numberChecker')
 
 describe('numberChecker', () => {
   test('returns true when number is 10', () => {
-    expect(numberChecker(10)).toEqual(true);
+    expect(numberChecker(545454)).toEqual(true);
   });
-  test('returns true when number is 1000', () => {
+  test.skip('returns true when number is 1000', () => {
     expect(numberChecker(1000)).toEqual(true);
   });
-  test('returns false when number is 9', () => {
+  test.skip('returns false when number is 9', () => {
     expect(numberChecker(9)).toEqual(false);
   });
-  test('returns false when number is 6', () => {
+  test.skip('returns false when number is 6', () => {
     expect(numberChecker(6)).toEqual(false);
   });
 });
